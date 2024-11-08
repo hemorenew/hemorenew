83546b5e7a55cbb4ca8f609b8313241f87c2f46e
Karen Obando <hemorenew@gmail.com>
2024-11-08 00:27:13 -0400

    [FEATURE] add pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,26 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head></Head>
+      <Head>
+        <link rel='manifest' href='manifest.json' suppressHydrationWarning />
+        <link rel='apple-touch-icon' href='/icons/icon-512x512.png' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='theme-color' content='#d8d8d8d8' />
+
+        <meta httpEquiv='X-Content-Type-Options' content='nosniff' />
+
+        <meta httpEquiv='X-Frame-Options' content='DENY' />
+        <meta httpEquiv='X-XSS-Protection' content='1; mode=block' />
+        <meta
+          httpEquiv='Strict-Transport-Security'
+          content='max-age=63072000; includeSubDomains; preload'
+        />
+        <meta name='referrer' content='no-referrer' />
+        <meta
+          name='permissions-policy'
+          content='geolocation=(), microphone=(), camera=()'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
